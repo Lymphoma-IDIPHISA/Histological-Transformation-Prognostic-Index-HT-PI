122669e787d2ec51f53f6c8b50f215b64755e2a9
Lymphoma Research Group <72218620+Lymphoma-IDIPHISA@users.noreply.github.com>
2024-05-15 11:01:23 +0200

    Update HT_predictive_model.R

--- HT_predictive_model.R
+++ HT_predictive_model.R
@@ -4,26 +4,24 @@
 DB = read.table("/file_folder/Example.txt", sep = "\t", header = T)
 
 DB$FLIPIcoef = c(0)
-DB$B2Mcoef = c(0)
 DB$KMT2Dcoef = c(0)
 DB$HIST1H1Ecoef = c(0)
 DB$TNFRSF14coef = c(0)
 
-x = which(DB$KMT2D == 0); DB$KMT2Dcoef[x] = 1.301285
-x = which(DB$HIST1H1E > 0); DB$HIST1H1Ecoef[x] = 2.070181
-x = which(DB$B2M > 0); DB$B2Mcoef[x] = 0.4126905
-x = which(DB$TNFRSF14 == 0); DB$TNFRSF14coef[x] = 1.546078
-x = which(DB$FLIPI > 0); DB$FLIPIcoef[x] = 1.608352
+x = which(DB$KMT2D == 0); DB$KMT2Dcoef[x] = 1.383305
+x = which(DB$HIST1H1E > 0); DB$HIST1H1Ecoef[x] = 2.102908
+x = which(DB$TNFRSF14 == 0); DB$TNFRSF14coef[x] = 1.615207
+x = which(DB$FLIPI > 0); DB$FLIPIcoef[x] = 1.639193
 
-DB$HT_model_Coef = c(0)
+DB$HT_model_Co = c(0)
 DB$HT_prob_2years = c(0)
 DB$HT_prob_5years = c(0)
 column_coef = grep(colnames(DB), pattern = "coef")
 
 for (l in 1:nrow(DB)) {
-DB$HT_model_Coef[l] = sum(DB[l,c(column_coef)])
-DB$HT_prob_2years[l] = 1-(0.9899^(exp(DB$HT_model_Coef[l])))
-DB$HT_prob_5years[l] = 1-(0.9603^(exp(DB$HT_model_Coef[l])))
+DB$HT_model_Co[l] = sum(DB[l,c(column_coef)])
+DB$HT_prob_2years[l] = 1-(0.9954^(exp(DB$HT_model_Co[l])))
+DB$HT_prob_5years[l] = 1-(0.9858^(exp(DB$HT_model_Co[l])))
 }
 
 DB = write.table(DB, "/file_folder/Output_HT_prediction.txt", sep = "\t", row.names = F)
